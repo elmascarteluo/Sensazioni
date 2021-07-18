0247624ed1910d2777a09f90346691b7073753c9
Luis Ruiz <luisruiz257@gmail.com>
2021-07-18 05:54:37 -0400

    Segunda pre entrega, añadido el carrito con events, storage, uso del DOM, se cambia las cards estáticas del HTML y son introducidas dinamicas desde el js, definicion de funciones y se libera el main js diviendo el codigo en varios archivos js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 //Toma nombre de Usuario y lo muestra en la navbar
-function showUser() {
+const showUser = () => {
     let userName = document.getElementById("userName").value;
     document.getElementById("userLogged").innerHTML = "Bienvenido, " + userName;  
 }
+
+//Carrito
+const cards = document.getElementById("cards");
+const items = document.getElementById("items");
+const footer = document.getElementById("footer");
+const templateCard = document.getElementById("template-card").content;
+const templateFooter = document.getElementById("template-footer").content;
+const templateCarrito = document.getElementById("template-carrito").content;
+const fragment = document.createDocumentFragment();
+let carrito = {};
+
+
+
