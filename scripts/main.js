715343135294e45a306b573d0ad6e2b9523493e3
Luis Ruiz <luisruiz257@gmail.com>
2021-08-17 18:07:09 -0400

    Indexado y cambios esteticos

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 $(".container-fluid").append(`<div id="carrito">
 								<button id="carritoButton">
-								<img id="carritoIcono" src="images/bag-2.svg" alt=""></button>
+								<img id="carritoIcono" src="../images/bag-2.svg" alt=""></button>
 								</div>`);
