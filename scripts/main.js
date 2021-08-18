d41df96559b02882bf031fe13ed5d7d01eaa8e6d
Luis Ruiz <luisruiz257@gmail.com>
2021-08-18 10:33:34 -0400

    cambio de cerrar carrito a un svg

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,7 @@ $(".container-fluid").append(`<div id="carrito">
 								<button id="carritoButton">
 								<img id="carritoIcono" src="../images/bag-2.svg" alt=""></button>
 								</div>`);
-								
+
+$("#cabeceraCarrito").append(`<button id="cerrarCarrito">
+								<img id="cerrarCarritoIcono" src="../images/close-circle.svg" alt=""></button>
+								`);
